22f7df524e91f85cb5dc3df82cfe669a4331a894
root <root@WhiteCmile>
2023-10-24 17:07:04 +0800

    upload a project folder as an example of CMake

---
+++ project/build/CMakeFiles/minivim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/mnt/d/Documents/Homework And Notes/Second Year/First semester/Programming TA/Minivim/bin/minivim"
+  "/mnt/d/Documents/Homework And Notes/Second Year/First semester/Programming TA/Minivim/bin/minivim.pdb"
+  "CMakeFiles/minivim.dir/src/ncurses_demo.cpp.o"
+  "CMakeFiles/minivim.dir/src/ncurses_demo.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minivim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ project/build/CMakeFiles/minivim.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for minivim.
+# This may be replaced when dependencies are built.
